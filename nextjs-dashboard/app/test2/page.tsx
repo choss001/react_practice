10d7447eed006f9ff3fd6454e088842a3dd14335
choss001 <choss01@naver.com>
2024-08-22 15:54:20 +0900

    make page

--- nextjs-dashboard/app/test2/page.tsx
+++ nextjs-dashboard/app/test2/page.tsx
@@ -1,13 +1,14 @@
+import Card from '@/app/ui/test2/card';
+
 export default function skeletons(){
     return(
         <div>
-            <div className="text-xl">Dash baord</div>
-            <div className="flex h-80 w-full">
-                <div className="grow bg-slate-300">temp</div>
-
-                <div className="grow bg-slate-300 ml-5">temp</div>
-                <div className="grow bg-slate-300 ml-5">temp</div>
-                <div className="grow bg-slate-300 ml-5">temp</div>
+            <div className="text-xl mb-9">Dash baord</div>
+            <div className="flex space-x-5">
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
+                <Card></Card>
             </div>
         </div>
     )
---
+++ nextjs-dashboard/app/ui/test2/card.tsx
@@ -0,0 +1,12 @@
+export default function Card(){
+    return (
+        <div className="px-2 grow bg-gray-50 w-64 rounded-xl">
+            <div>
+                Jo seong sik
+            </div>
+            <div className="mx-1 my-1 px-7 py-5 bg-white rounded-xl">
+                234,405$
+            </div>
+        </div>
+    )
+}
